55e607b23bf763c027e828ba87d58d90f569481b
Luebu <maximilian.luecker@hebk.de>
2022-10-20 14:58:42 +0200

    initial commit

--- Aufgabe1/src/Zufallszahl.java
+++ Aufgabe1/src/Zufallszahl.java
@@ -1,12 +1,15 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Zufallszahl {
     Scanner sc = new Scanner(System.in);
     int zufallszahl;
     int versuche;
+    Random rn = new Random();
 
     public void raten(){
-        zufallszahl=(int)Math.random()*sc.nextInt()+1;
+        zufallszahl=rn.nextInt(sc.nextInt());
         System.out.println(zufallszahl);
     }
+
 }
