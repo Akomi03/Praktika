31c5e295af95c14700f4f849f4bf61c568114627
Luebu <maximilian.luecker@hebk.de>
2022-10-26 14:32:09 +0200

    Aufgabe 2 init commit

---
+++ Aufgabe2/src/Palindrom.java
@@ -0,0 +1,27 @@
+public class Palindrom {
+    String[] woerter;
+
+    public static void main(String[] args) {
+        Palindrom p = new Palindrom();
+
+
+    }
+
+    public void einlesen(String input) {
+        woerter = input.split(" ");
+
+    }
+
+    public void testen() {
+        String wort;
+        for (int i = 0; i <= woerter.length; i++) {
+            char[] charword = woerter[i].toCharArray();
+            char[] palindrom = new char[charword.length];
+            for (int j = 0; )
+
+
+        }
+
+    }
+
+}
