9e94d887b11c7313ad94d5e3de058741f55fcd69
Luebu <maximilian.luecker@hebk.de>
2022-10-27 14:19:36 +0200

    init comit test module

---
+++ Test/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> test = new ArrayList<Integer>();
+        for (int i=0;i<=1000;i++){
+            test.add(i);
+        }
+        System.out.println(test);
+    }
+}
