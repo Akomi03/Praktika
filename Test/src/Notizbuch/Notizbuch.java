79ca7361e7f409cba9ea9c5d6067b13da5f90fb4
Luebu <maximilian.luecker@hebk.de>
2022-11-03 14:25:25 +0100

    Format

--- Test/src/Notizbuch/Notizbuch.java
+++ Test/src/Notizbuch/Notizbuch.java
@@ -11,72 +11,75 @@ public class Notizbuch {
         name = pName;
     }
 
-    public void add(String pTitel,String pNotiz){
-        buch.add(new Notiz(pNotiz,pTitel));
+    public void add(String pTitel, String pNotiz) {
+        buch.add(new Notiz(pNotiz, pTitel));
     }
-    public void add(String pTitel, String pNotiz, LocalDate pDate){
-        buch.add(new Notiz(pNotiz,pTitel,pDate));
+
+    public void add(String pTitel, String pNotiz, LocalDate pDate) {
+        buch.add(new Notiz(pNotiz, pTitel, pDate));
     }
-    public void list(){
+
+    public void list() {
         Notiz[] b = new Notiz[buch.size()];
-        for (int i =0;i<buch.size();i++){
-            b[i]=buch.get(i);
+        for (int i = 0; i < buch.size(); i++) {
+            b[i] = buch.get(i);
         }
-        for(Notiz arr:b){
-            if(arr.getFaelligkeit()!=null) {
+        for (Notiz arr : b) {
+            if (arr.getFaelligkeit() != null) {
                 System.out.println(arr.getTitel() + " | " + arr.getFaelligkeit());
-            }else{
+            } else {
                 System.out.println(arr.getTitel() + " | kein Datum Vorhanden");
             }
             System.out.println();
         }
     }
-    public void view(String pTitel){
-        String titel="";
-        int i=0;
-        while (!titel.equals(pTitel)){
-            if(i>= buch.size()){
+
+    public void view(String pTitel) {
+        String titel = "";
+        int i = 0;
+        while (!titel.equals(pTitel)) {
+            if (i >= buch.size()) {
                 System.out.println("Keine Notiz vorhanden");
                 return;
             }
-            titel=buch.get(i).getTitel();
+            titel = buch.get(i).getTitel();
             i++;
         }
-        System.out.println(buch.get(i-1).getTitel()+" | "+buch.get(i-1).getNotiz());
+        System.out.println(buch.get(i - 1).getTitel() + " | " + buch.get(i - 1).getNotiz());
 
     }
-    public void delete(String pTitel){
-        String titel="";
-        int i=0;
-        while (!titel.equals(pTitel)){
-            if(i>= buch.size()){
+
+    public void delete(String pTitel) {
+        String titel = "";
+        int i = 0;
+        while (!titel.equals(pTitel)) {
+            if (i >= buch.size()) {
                 System.out.println("Keine Notiz vorhanden");
                 return;
             }
-            titel=buch.get(i).getTitel();
+            titel = buch.get(i).getTitel();
             i++;
         }
-        System.out.println("Notiz mit Titel "+pTitel+" erfolgreich geloescht");
-        buch.remove(i-1);
+        System.out.println("Notiz mit Titel " + pTitel + " erfolgreich geloescht");
+        buch.remove(i - 1);
 
     }
-    public void edit(String pTitel,String pNew){
-        String titel="";
-        int i=0;
-        while (!titel.equals(pTitel)){
-            if(i>= buch.size()){
+
+    public void edit(String pTitel, String pNew) {
+        String titel = "";
+        int i = 0;
+        while (!titel.equals(pTitel)) {
+            if (i >= buch.size()) {
                 System.out.println("Keine Notiz vorhanden");
                 return;
             }
-            titel=buch.get(i).getTitel();
+            titel = buch.get(i).getTitel();
             i++;
         }
-        buch.get(i-1).setNotiz(pNew);
-        System.out.println(buch.get(i-1).getTitel()+" | "+buch.get(i-1).getNotiz());
+        buch.get(i - 1).setNotiz(pNew);
+        System.out.println(buch.get(i - 1).getTitel() + " | " + buch.get(i - 1).getNotiz());
 
     }
-
-
 
 
     public String getName() {
